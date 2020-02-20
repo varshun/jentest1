0f3f325d6c033b48f9a5d2665c01138ca4068fce
Varshun <varshun.tiku@nihilent.com>
2020-02-20 12:22:18 +0530

    Addition.cpp added to repo

---
+++ Addition.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+class add
+{
+	void add (int a, int b)
+	{
+		int c;
+		c= a+b;
+		cout<<"Addition=",c<<endl;
+	}
+	void sub (int a, int b)
+	{
+		int c;
+		c= a-b;
+		cout<<"Subtraction=",c<<endl;
+	}
+	  
+	
+}
